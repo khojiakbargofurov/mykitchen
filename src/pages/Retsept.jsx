adf2a97a0d2d6cc54e2cdc97f75d689b1cfb826c
Khojiakbar <harry.gafurov@gmail.com>
2024-07-11 00:15:10 +0500

    detailspage updated

--- src/pages/Retsept.jsx
+++ src/pages/Retsept.jsx
@@ -54,7 +54,7 @@ function Retsept() {
                   <img src={KitchenIcon} alt="kitchen icon" />Meal Name: {dataId?.title}
                 </h2>
                 <div className="mb-5 flex items-start gap-2">
-                  <span className="font-semibold flex gap-2">
+                  <span className="font-semibold  text-xl  flex gap-2">
                     <ShoppingBasket />
                     Ingrediens: </span>
                   <ul className="flex flex-wrap gap-2">
@@ -71,7 +71,7 @@ function Retsept() {
                   </ul>
                 </div>
                 <div className="mb-5">
-                  <span className="font-semibold flex gap-2 items-center">
+                  <span className="font-semibold  text-xl  flex gap-2 items-center">
                     <Clock />
                     Cooking time:
                     <span className="ml-2 font-normal">
@@ -80,18 +80,18 @@ function Retsept() {
                   </span>
                 </div>
                 <div className="mb-5">
-                  <span className="font-semibold flex gap-2">
+                  <span className="font-semibold  text-xl  flex gap-2">
                     <CircleDollarSign />
                     Prise:
                     <span className="ml-2 font-normal">{dataId?.price} so'm</span>
                   </span>
                 </div>
-                <div className="flex gap-2">
-                  <h3 className="mb-2 font-semibold flex gap-2"><BookType /> Nation:</h3>
-                  <p className="mb-5">{dataId?.nation}</p>
+                <div className="flex gap-2 items-top">
+                  <h3 className="mb-2 font-semibold text-xl flex gap-2"><BookType /> Nation:</h3>
+                  <p className="mb-5 text-xl">{dataId?.nation} meal</p>
                 </div>
                 <h3 className="mb-2 text-xl font-semibold flex gap-2 items-center"><NotebookText />Method:</h3>
-                <p className="mb-5 ">{dataId?.description}</p>
+                <p className="mb-5 text-xl">{dataId?.description}</p>
                 <div className="sm:flex bleck items-center w-full justify-between">
                   <div className="flex items-center gap-2 sm:mb-0 mb-10">
                     <button
